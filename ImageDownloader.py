8005c64185657c8dcd94a1d0d511cedbfd8a0cc6
jaykhatri73 <khatri.jay73@gmail.com>
2023-08-05 13:45:02 -0400

    downloader code

---
+++ ImageDownloader.py
@@ -0,0 +1,31 @@
+import urllib.request
+import os
+
+
+def download_image(url, save_directory):
+    # Create the save directory if it doesn't exist
+    os.makedirs(save_directory, exist_ok=True)
+
+    # Get the filename from the URL
+    filename = os.path.basename(url)
+
+    try:
+        # Download the image
+        save_path = os.path.join(save_directory, filename)
+
+        # Print the save path for debugging
+        print('Save path:', save_path)
+        urllib.request.urlretrieve(url, save_path)
+        print('Image downloaded successfully!')
+    except urllib.error.URLError:
+        print('Failed to download image.')
+    except IOError:
+        print('Failed to save image.')
+
+
+image_url = input('Enter the image URL or local file path: ')
+save_directory = input(
+    "Give your save path(Copy file directory from the computer): ")
+
+
+download_image(image_url, save_directory)
